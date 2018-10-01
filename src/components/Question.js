4b4134cc0cb3e078b8199b51c51a9817819c9492
Matt Winzer <matt.winzer@gmail.com>
2018-10-01 14:54:45 -0600

    added confirmation modal when question completed button clicked

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,29 +1,63 @@
-import React from 'react'
-import { Card, Icon, Button } from 'semantic-ui-react'
+import React, { Component } from 'react'
+import { Card, Icon, Button, Modal, Header } from 'semantic-ui-react'
 
-const Question = ({ question, removeQuestion, toggleContacted }) => {
-  const { id, name, location, googled, askedStudent, hasDebugged, contacted } = question
-  
-  return (
-    <Card>
-      <Card.Content><Card.Header>{name} - {location}</Card.Header></Card.Content>
-      <Card.Content description={question.question} />
-      <Card.Content extra>
-        {googled ? <Icon circular color='teal' name='google' /> : <Icon circular name='google' />}
-        {askedStudent ? <Icon circular color='teal' name='talk' /> : <Icon circular name='talk' />}
-        {hasDebugged ? <Icon circular color='teal' name='bug' /> : <Icon circular name='bug' />}
-        <Button.Group floated="right">
-          <Button compact icon labelPosition='left' positive={contacted} onClick={() => {toggleContacted(id, contacted)}}>
-            <Icon name='right arrow' />
-            Contacted
-          </Button>
-          <Button compact icon color="teal" labelPosition='right' onClick={() => {removeQuestion(id)}}>
-            Completed
-            <Icon name='check' />
-          </Button>
-        </Button.Group>
-      </Card.Content>
-    </Card>
-  )
+class Question extends Component {
+  state = { modalOpen: false }
+
+  handleOpen = () => this.setState({ modalOpen: true })
+  handleClose = () => this.setState({ modalOpen: false })
+  handleComplete = (id) => {
+    this.props.removeQuestion(id)
+    this.handleClose()
+  }
+   
+  render() {
+    const { question, removeQuestion, toggleContacted } = this.props
+    const { id, name, location, googled, askedStudent, hasDebugged, contacted } = question
+
+    return (
+      <Card>
+        <Card.Content><Card.Header>{name} - {location}</Card.Header></Card.Content>
+        <Card.Content description={question.question} />
+        <Card.Content extra>
+          {googled ? <Icon circular color='teal' name='google' /> : <Icon circular name='google' />}
+          {askedStudent ? <Icon circular color='teal' name='talk' /> : <Icon circular name='talk' />}
+          {hasDebugged ? <Icon circular color='teal' name='bug' /> : <Icon circular name='bug' />}
+          <Button.Group floated="right">
+            <Button compact icon labelPosition='left' positive={contacted} onClick={() => {toggleContacted(id, contacted)}}>
+              <Icon name='right arrow' />
+              Contacted
+            </Button>
+            <Button compact icon color="teal" labelPosition='right' onClick={this.handleOpen}>
+              Completed
+              <Icon name='check' />
+            </Button>
+          </Button.Group>
+        </Card.Content>
+        
+        <Modal
+          open={this.state.modalOpen}
+          onClose={this.handleClose}
+          size='small'
+        >
+          <Header icon='warning sign' content='Confirm Completion' />
+          <Modal.Content>
+            <h3>Are you sure this question has been completed?</h3>
+          </Modal.Content>
+          <Modal.Actions>
+            <Button color='red' onClick={this.handleClose} inverted>
+              <Icon name='undo' />
+              Abort
+            </Button>
+            <Button color='green' onClick={() => this.handleComplete(id)} inverted>
+              <Icon name='checkmark' />
+              Yup
+            </Button>
+          </Modal.Actions>
+        </Modal>
+      </Card>
+    )
+  }
 }
+
 export default Question
