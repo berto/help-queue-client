2a68129636dcb8a9b555b09f8d9388af25d3eb1b
Matt Winzer <matt.winzer@gmail.com>
2018-10-02 12:20:45 -0600

    refactored Header into functional component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,12 @@
 import React, { Component } from 'react'
 import { Menu } from 'semantic-ui-react'
 
-class Header extends Component {
-  render() {
-    return (
-      <Menu inverted>
-        <Menu.Item header>
-          <h1>gSchool Help Queue</h1>
-        </Menu.Item>
-      </Menu>
-    )
-  }
-}
+const Header = () => (
+  <Menu inverted>
+    <Menu.Item header>
+      <h1>gSchool Help Queue</h1>
+    </Menu.Item>
+  </Menu>
+)
 
 export default Header
